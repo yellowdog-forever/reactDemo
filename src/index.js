a5f6da691d5a51b24950994cab42edb25438806d
shujunqian <17602156892@163.com>
2020-02-16 00:14:51 +0800

    m

--- src/index.js
+++ src/index.js
@@ -2,22 +2,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// 为什么用类的方法来写就要用this.state呢
+// 为什么只有在constructor里面可以用this.state = xxx 呢
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="Edite" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+ 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      count: 0,
+      cloneCount: 0
+    };
+  }
+  componentDidMount() {
+    this.timeId = setInterval(() => {
+      this.setState({date: new Date()})
+    }, 1000);
+
+    // this.setState({
+    //   count: this.state.count + 1
+    // })
+    // this.setState({
+    //   count: this.state.count + 1
+    // })
+    // this.setState({
+    //   count: this.state.count + 1
+    // })
+    // this.setState({
+    //   count: this.state.count + 1
+    // })
+    // setTimeout(() => {
+      this.setState((state) => ({
+        count:state.count + 1
+      }))
+      this.setState((state) => ({
+        count:state.count + 1
+      }))
+      this.setState((state) => ({
+        count:state.count + 1
+      }))
+    // }, 1000)
+
+    setTimeout(() => {
+      this.setState((state) => ({
+        cloneCount:state.cloneCount + 1
+      }))
+    }, 2000)
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeId);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>count {this.state.count}, {this.state.cloneCount}</h2>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-  <App />,
+  <Clock />,
   document.getElementById('root')
 );
