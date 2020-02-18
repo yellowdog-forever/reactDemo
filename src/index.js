41359ab9a2f6124d4792631bda396b8690cebf45
shujunqian <17602156892@163.com>
2020-02-18 23:43:58 +0800

    m

--- src/index.js
+++ src/index.js
@@ -2,90 +2,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// 为什么用类的方法来写就要用this.state呢
-// 为什么只有在constructor里面可以用this.state = xxx 呢
 
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
+const FancyButton = React.forwardRef((props, ref) => (
+  <button ref={ref} className="FancyButton">
+    {props.children}
+  </button>
+));
+// vue可以在组件上面绑定click方法
 
-// function App() {
-//   return (
-//     <div>
-//       <Welcome name="Sara" />
-//       <Welcome name="Cahal" />
-//       <Welcome name="Edite" />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
- 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
+class A extends React.Component{
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
     this.state = {
-      date: new Date(),
-      count: 0,
-      cloneCount: 0
-    };
+      ref: React.createRef()
+    }
   }
-  componentDidMount() {
-    this.timeId = setInterval(() => {
-      this.setState({date: new Date()})
-    }, 1000);
-
-    // this.setState({
-    //   count: this.state.count + 1
-    // })
-    // this.setState({
-    //   count: this.state.count + 1
-    // })
-    // this.setState({
-    //   count: this.state.count + 1
-    // })
-    // this.setState({
-    //   count: this.state.count + 1
-    // })
-    // setTimeout(() => {
-      this.setState((state) => ({
-        count:state.count + 1
-      }))
-      this.setState((state) => ({
-        count:state.count + 1
-      }))
-      this.setState((state) => ({
-        count:state.count + 1
-      }))
-    // }, 1000)
-
-    setTimeout(() => {
-      this.setState((state) => ({
-        cloneCount:state.cloneCount + 1
-      }))
-    }, 2000)
-
+  
+  handleClick() {
+    console.log(">>>>>>>>>>>>>>>", this.state.ref)
   }
-
-  componentWillUnmount() {
-    clearInterval(this.timeId);
-  }
-
   render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <h2>count {this.state.count}, {this.state.cloneCount}</h2>
-      </div>
-    );
+    return <div onClick={this.handleClick}><FancyButton ref={this.state.ref}>ddd</FancyButton></div>
   }
 }
 
+// 你可以直接获取 DOM button 的 ref：
+
 ReactDOM.render(
-  <Clock />,
+  <A></A>,
   document.getElementById('root')
 );
